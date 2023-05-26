e9a891da666686398aea7ddcca60890f2a1860b8
DESKTOP-DJ68C5A\Ian <mano96@live.com.mx>
2023-05-25 21:35:00 -0500

    cambios finales ohala

--- redvitab/settings.py
+++ redvitab/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,6 +77,7 @@ WSGI_APPLICATION = 'redvitab.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
 
 DATABASES = {
      'default': {
